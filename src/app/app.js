ab6e50582bc75f4b051626177662d09fbc84975d
zoltan-antal <zoltan_antal@outlook.com>
2023-07-13 15:34:08 +0200

    Set up main() function skeleton

---
+++ src/app/app.js
@@ -0,0 +1,9 @@
+import Todo from "./todo";
+import Project from "./project";
+import ProjectHandler from "./project-handler";
+
+const projectHandler = new ProjectHandler;
+
+export default function main() {
+
+}
