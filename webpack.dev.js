74ec887e67c5fc7906353d48f82d45472240d668
zoltan-antal <zoltan_antal@outlook.com>
2023-07-13 17:28:49 +0200

    Edit Webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    static: './dist',
+    static: './',
   },
   output: {
     filename: 'main.js',
