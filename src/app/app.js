695c1a9c61ff5c14459d3c2361e5dd3df96123df
zoltan-antal <zoltan_antal@outlook.com>
2023-07-17 16:18:52 +0200

    Style task dialog

--- src/app/app.js
+++ src/app/app.js
@@ -5,5 +5,11 @@ import ProjectHandler from "./project-handler";
 const projectHandler = new ProjectHandler;
 
 export default function main() {
+  const addTodoDialog = document.querySelector(".todo-dialog");
+  const addTodoButton = document.querySelector(".add-todo");
+  addTodoButton.addEventListener("click", () => {
+    addTodoDialog.showModal();
+  });
+
 
 }
