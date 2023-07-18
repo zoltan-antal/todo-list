5c5315670069a8752ff448a0d7690f7c37f72305
zoltan-antal <zoltan_antal@outlook.com>
2023-07-18 13:06:21 +0200

    Clean up todo data structure

--- src/app/classes/todo.js
+++ src/app/classes/todo.js
@@ -1,14 +1,18 @@
 export default class Todo {
   static nextId = 0;
 
-  constructor({title, description, dueDate, priority}) {
+  constructor({title, description, dueDate, priority, isComplete = false}) {
     this.id = Todo.nextId;
     Todo.nextId++;
 
     this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
+    if (description) {
+      this.description = description;
+    }
+    if (dueDate) {
+      this.dueDate = new Date(dueDate.year, dueDate.month, dueDate.day);
+    }
     this.priority = priority;
-    this.isComplete = false;
+    this.isComplete = isComplete;
   }
 }
